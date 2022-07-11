69f944140f7c6e75a06bc812ae0ba65a77e6a202
ibmsc <Ibm.sc1409@gmail.com>
2022-07-11 11:12:30 +0300

    HW Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author authorTolstoy = new Author("Лев ","Толстой");
+        Book bookWarAndPeace = new Book("Война и мир", 2014, authorTolstoy);
+
+        Author authorPyshkin = new Author("Александр ", "Пушкин");
+        Book bookSkazki = new Book(("Сказки"), 1999, authorPyshkin);
+
+        System.out.println("Книга №1 - " + bookWarAndPeace.getName() + "; Год издания - " + bookWarAndPeace.getYear()+
+                "; Автор книги - " + bookWarAndPeace.getAuthor().getFirstNameAuthor() + " " + bookWarAndPeace.getAuthor().getSurnameAuthor()+".");
+
+        System.out.println();
+        System.out.println("Книга №2 - " + bookSkazki.getName()+ "; Год издания - " + bookSkazki.getYear()+
+                "; Автор книги - " + bookSkazki.getAuthor().getFirstNameAuthor() + " " + bookSkazki.getAuthor().getSurnameAuthor() + ".");
+        System.out.println();
+
+        bookWarAndPeace.setYear(2016);
+        System.out.println("Книга №1 - " + bookWarAndPeace.getName() + "; Год издания - " + bookWarAndPeace.getYear()+
+                "; Автор книги - " + bookWarAndPeace.getAuthor().getFirstNameAuthor() + " " + bookWarAndPeace.getAuthor().getSurnameAuthor()+".");
     }
 }
